3be421695e4882d032bb6b1c186c67fadc1cc38e
mcopik <mcopik@gmail.com>
2016-12-28 13:08:31 +0100

    Tags for different libraries

---
+++ include/libraries.hpp
@@ -0,0 +1,25 @@
+
+
+#ifndef LINALG_TESTS_LIBRARIES_HPP
+#define LINALG_TESTS_LIBRARIES_HPP
+
+#include <armadillo>
+#include <Eigen/Dense>
+#include <blaze/Blaze.h>
+#include <boost/numeric/mtl/mtl.hpp>
+// Required for mtl::traits::eval
+#include <boost/numeric/mtl/utility/eval.hpp>
+
+namespace library {
+
+    struct eigen {};
+    
+    struct blaze {};
+
+    struct mtl4 {};
+
+    struct arma {};
+
+}
+
+#endif
