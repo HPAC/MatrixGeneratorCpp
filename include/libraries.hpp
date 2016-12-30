1be787c535b00a8680eea4436a5f090bcdf04c0a
mcopik <mcopik@gmail.com>
2016-12-30 10:27:09 +0100

    Update licensing

--- include/libraries.hpp
+++ include/libraries.hpp
@@ -1,4 +1,7 @@
-
+//  Copyright (c) 2016 Marcin Copik
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #ifndef LINALG_TESTS_LIBRARIES_HPP
 #define LINALG_TESTS_LIBRARIES_HPP
