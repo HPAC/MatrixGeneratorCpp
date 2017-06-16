4e64d33646f4ab30e97b89ad07520d4c8c82d2c5
mcopik <mcopik@gmail.com>
2017-06-16 18:31:02 +0200

    Add missing file

---
+++ julia/Generator.jl/tests/band_types.jl
@@ -0,0 +1,35 @@
+
+function generate_band_types()
+
+  shape_types = Set()
+  push!(shape_types, ([Shape.General(2, 2)], Shape.General(2, 2)))
+  push!(shape_types, ([Shape.General(3, 2)], Shape.General(3, 2)))
+  # Symmetric
+  push!(shape_types, ([Shape.Symmetric(3)], Shape.Symmetric(3)))
+  push!(shape_types, ([Shape.Symmetric(3), Shape.General(3, 3)], Shape.Symmetric(3)))
+  # Triangular
+  push!(shape_types, ([Shape.Triangular(3, Shape.Upper), Shape.General(3, 3)], Shape.Triangular(3, Shape.Upper)))
+  push!(shape_types, ([Shape.Triangular(4, Shape.Lower), Shape.General(4, 4)], Shape.Triangular(4, Shape.Lower)))
+  push!(shape_types, ([Shape.Triangular(4, Shape.Lower), Shape.Triangular(4, Shape.Upper)], Shape.Diagonal(4)))
+  # Band non-symmetric
+  push!(shape_types, ([Shape.General(1, 1)], Shape.Band(1, 1, 0, 0, false)))
+  push!(shape_types, ([Shape.General(2, 2), Shape.Band(2, 2, 1, 0, false)], Shape.Band(2, 2, 1, 0, false)))
+  push!(shape_types, ([Shape.General(3, 2), Shape.Band(3, 2, 1, 1, false)], Shape.Band(3, 2, 1, 1, false)))
+  push!(shape_types, ([Shape.General(3, 2), Shape.Band(3, 2, 0, 1, false)], Shape.Band(3, 2, 0, 1, false)))
+  push!(shape_types, ([Shape.General(5, 4), Shape.Band(5, 4, 4, 3, false)], Shape.Band(5, 4, 4, 3, false)))
+  # band sizes with very non-square matrices
+  push!(shape_types, ([Shape.General(1, 3), Shape.Band(1, 3, 0, 1, false)], Shape.Band(1, 3, 0, 1, false)))
+  push!(shape_types, ([Shape.General(3, 1), Shape.Band(3, 1, 1, 0, false)], Shape.Band(3, 1, 1, 0, false)))
+  push!(shape_types, ([Shape.General(5, 2), Shape.Band(5, 2, 0, 1, false)], Shape.Band(5, 2, 0, 1, false)))
+  push!(shape_types, ([Shape.General(2, 5), Shape.Band(2, 5, 1, 0, false)], Shape.Band(2, 5, 1, 0, false)))
+  push!(shape_types, ([Shape.General(5, 4), Shape.Band(5, 4, 4, 3, false), Shape.Band(5, 4, 2, 3, false)], Shape.Band(5, 4, 2, 3, false)))
+  push!(shape_types, ([Shape.Triangular(5, Shape.Upper), Shape.Band(5, 5, 4, 3, false), Shape.Band(5, 5, 2, 3, false)], Shape.Band(5, 5, 0, 3, false)))
+  push!(shape_types, ([Shape.Triangular(5, Shape.Lower), Shape.Band(5, 5, 4, 3, false), Shape.Band(5, 5, 2, 3, false)], Shape.Band(5, 5, 2, 0, false)))
+  # Band symmetric
+  push!(shape_types, ([Shape.Symmetric(1)], Shape.Band(1, 1, 0, 0, true)))
+  push!(shape_types, ([Shape.Symmetric(5), Shape.Band(5, 5, 4, 4, false)], Shape.Band(5, 5, 4, 4, true)))
+  push!(shape_types, ([Shape.Symmetric(5), Shape.Band(5, 5, 4, 3, false), Shape.Band(5, 5, 2, 2, false)], Shape.Band(5, 5, 2, 2, true)))
+  push!(shape_types, ([Shape.Diagonal(5), Shape.Band(5, 5 , 4, 3, false), Shape.Band(5, 5, 2, 3, false)], Shape.Band(5, 5, 0, 0, true)))
+
+  return shape_types
+end
