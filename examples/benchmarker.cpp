11717062390947cba1001139341df6afc4631723
mcopik <mcopik@gmail.com>
2017-08-08 17:44:17 +0200

    Extend CMake support to Blaze

--- examples/benchmarker.cpp
+++ examples/benchmarker.cpp
@@ -8,76 +8,80 @@
 #include <random>
 
 #include <benchmarker/benchmarker.hpp>
-#include <interfaces/libraries.hpp>
+#include <libraries.hpp>
 
 using linalg_tests::benchmarker;
 
-
-
-/// Blaze kernel as a function
-/// Random initialization takes more time due to:
-/// a) lack of randomly initialized matrices
-/// b) unnecessary copy caused by forEach being an expression
-///   evaluated on assignment
-/// \param b
-/// \param rows
-/// \param cols
-/// \return
-void blaze_kernel(benchmarker & b, int rows, int cols)
-{
-    int idx = b.add_clock();
-    b.start_clock( idx );
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<> dis(0, 1);
-    blaze::DynamicMatrix<double> A(rows, cols);
-    A = blaze::forEach(A, [&](double) { return dis(gen); });
-    blaze::DynamicMatrix<double> B(rows, cols);
-    B = blaze::forEach(B, [&](double) { return dis(gen); });
-    b.stop_clock(idx);
-
-    b.start_clock(b.add_clock());
-    // TODO: remove when better non-lazy evaluation is implemented
-    blaze::DynamicMatrix<double> C = blaze::eval(A * blaze::trans(B));
-    // Use the feature of stopping last clock
-    b.stop_clock();
-}
-
-/// Eigen kernel as a function object
-struct eigen_kernel
-{
-    benchmarker & b;
-
-    eigen_kernel(benchmarker & b_) :
-            b(b_) {}
-
-    void operator()(int rows, int cols)
+#ifdef HAVE_BLAZE
+    /// Blaze kernel as a function
+    /// Random initialization takes more time due to:
+    /// a) lack of randomly initialized matrices
+    /// b) unnecessary copy caused by forEach being an expression
+    ///   evaluated on assignment
+    /// \param b
+    /// \param rows
+    /// \param cols
+    /// \return
+    void blaze_kernel(benchmarker & b, int rows, int cols)
     {
-        // Note that we can rely on increasing indices, remembering is not necessary
-        b.start_clock( b.add_clock() );
-        Eigen::MatrixXd A = Eigen::MatrixXd::Random(rows, cols);
-        Eigen::MatrixXd B = Eigen::MatrixXd::Random(rows, cols);
-        // Use the feature of stopping last clock
-        b.stop_clock();
+        int idx = b.add_clock();
+        b.start_clock( idx );
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<> dis(0, 1);
+        blaze::DynamicMatrix<double> A(rows, cols);
+        A = blaze::forEach(A, [&](double) { return dis(gen); });
+        blaze::DynamicMatrix<double> B(rows, cols);
+        B = blaze::forEach(B, [&](double) { return dis(gen); });
+        b.stop_clock(idx);
 
         b.start_clock(b.add_clock());
-        auto C = (A * B.transpose()).eval();
+        // TODO: remove when better non-lazy evaluation is implemented
+        blaze::DynamicMatrix<double> C = blaze::eval(A * blaze::trans(B));
+        // Use the feature of stopping last clock
         b.stop_clock();
     }
+#endif
 
-};
+#ifdef HAVE_EIGEN
+    /// Eigen kernel as a function object
+    struct eigen_kernel
+    {
+        benchmarker & b;
 
-void arma_kernel(benchmarker & b, int rows, int cols)
-{
-    b.start_clock(0);
-    arma::mat A = arma::randu<arma::mat>(rows, cols);
-    arma::mat B = arma::randu<arma::mat>(rows, cols);
-    b.stop_clock(0);
+        eigen_kernel(benchmarker & b_) :
+                b(b_) {}
 
-    b.start_clock(1);
-    auto C = (A * B.t() ).eval();
-    b.stop_clock(1);
-}
+        void operator()(int rows, int cols)
+        {
+            // Note that we can rely on increasing indices, remembering is not necessary
+            b.start_clock( b.add_clock() );
+            Eigen::MatrixXd A = Eigen::MatrixXd::Random(rows, cols);
+            Eigen::MatrixXd B = Eigen::MatrixXd::Random(rows, cols);
+            // Use the feature of stopping last clock
+            b.stop_clock();
+
+            b.start_clock(b.add_clock());
+            auto C = (A * B.transpose()).eval();
+            b.stop_clock();
+        }
+
+    };
+#endif
+
+#ifdef HAVE_ARMADILLO
+    void arma_kernel(benchmarker & b, int rows, int cols)
+    {
+        b.start_clock(0);
+        arma::mat A = arma::randu<arma::mat>(rows, cols);
+        arma::mat B = arma::randu<arma::mat>(rows, cols);
+        b.stop_clock(0);
+
+        b.start_clock(1);
+        auto C = (A * B.t() ).eval();
+        b.stop_clock(1);
+    }
+#endif
 
 int main()
 {
@@ -87,15 +91,20 @@ int main()
     int iters = 100;
 
     // Run a function with additional args
+#ifdef HAVE_BLAZE
     benchmark.run(iters, blaze_kernel, rows, cols);
+#endif
 
     // Run a functor without benchmark arg
+#ifdef HAVE_EIGEN
     eigen_kernel k(benchmark);
     benchmark.run(iters, k, rows, cols);
+#endif
 
     // Allocate clocks before run
     // Run a lambda expression without args - capture everything
     benchmark.reserve_clocks(2);
+#ifdef HAVE_ARMADILLO
     benchmark.run(iters,
                   [=, &benchmark]() {
                       benchmark.start_clock(0);
@@ -107,7 +116,9 @@ int main()
                       auto C = (A * B.t() ).eval();
                       benchmark.stop_clock(1);
                   });
+#endif
 
+#ifdef HAVE_MTL4
     benchmark.run(iters,
                   [=, &benchmark]() {
                       benchmark.start_clock(0);
@@ -130,6 +141,7 @@ int main()
                       auto C = mtl::traits::evaluate(A * mtl::mat::trans(B) );
                       benchmark.stop_clock(1);
                   });
+#endif
 
     std::array<std::string, 4> labels{"Blaze", "Eigen", "Armadillo", "MTL4"};
     std::array<std::string, 2> clock_labels{"Initialization", "Computation"};
--- include/libraries.hpp
+++ include/libraries.hpp
@@ -6,22 +6,41 @@
 #ifndef LINALG_TESTS_LIBRARIES_HPP
 #define LINALG_TESTS_LIBRARIES_HPP
 
+#ifdef HAVE_ARMADILLO
 #include <armadillo>
+#endif
+
+#ifdef HAVE_EIGEN
 #include <Eigen/Dense>
+#endif
+
+#ifdef HAVE_BLAZE
 #include <blaze/Blaze.h>
+#endif
+
+#ifdef HAVE_MTL4
 #include <boost/numeric/mtl/mtl.hpp>
 // Required for mtl::traits::eval
 #include <boost/numeric/mtl/utility/eval.hpp>
+#endif
 
 namespace library {
 
+#ifdef HAVE_EIGEN
     struct eigen {};
+#endif
     
+#ifdef HAVE_BLAZE
     struct blaze {};
+#endif
 
+#ifdef HAVE_MTL4
     struct mtl4 {};
+#endif
 
+#ifdef HAVE_ARMADILLO
     struct arma {};
+#endif
 
 }
 
