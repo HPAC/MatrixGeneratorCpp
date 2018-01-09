67e58feb81ce56ebcf94303284731493f7be1fa7
mcopik <mcopik@gmail.com>
2018-01-09 13:44:46 +0100

    Remove Julia code shipped to seperate package

--- examples/julia/generator.jl
+++
@@ -1,7 +0,0 @@
-workspace()
-reload("Generator")
-using Generator
-
-println(generate(Shape.General(2, 2), Set([Properties.Random(0, 3)])))
-println(generate(Shape.General(2, 2), Set([ Properties.Negative, Properties.Random(-2, -1)])))
-println(generate(Shape.Symmetric(2), Set([Properties.Random, Properties.Positive])))
